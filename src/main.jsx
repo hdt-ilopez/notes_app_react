d57846cd1cd5b9503b73a0153116730f007f0007
Izaac Lopez <ilopez@illuminateit.us>
2024-09-02 04:37:32 -0500

    removed notes provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
-import { NotesProvider } from "./contexts/NotesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <NotesProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </NotesProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
   </AuthProvider>
 );
