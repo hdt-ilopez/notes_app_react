aadf6ad946e11b91cf2da8d6ae5b1c46a7365016
Izaac Lopez <ilopez@illuminateit.us>
2024-09-02 04:37:49 -0500

    Added notes provider

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,37 +3,40 @@ import { useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import CreateNoteButton from "../components/CreateNoteButton";
+import { NotesProvider } from "../contexts/NotesContext";
 
 const MainLayout = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
   return (
-    <div
-      style={{
-        display: isNonMobile ? "flex" : "block",
-        width: "100%",
-        height: "100%",
-        overflow: "hidden",
-      }}
-    >
+    <NotesProvider>
       <div
-        className="bg-gray-100"
-        style={{ flexGrow: 1, overflow: "hidden", maxHeight: "100vh" }}
+        style={{
+          display: isNonMobile ? "flex" : "block",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden",
+        }}
       >
-        <Navbar
-          style={{
-            position: "absolute",
-            top: 0,
-            width: "100%",
-            zIndex: 9999,
-          }}
-        />
-        <div className={isNonMobile ? "px-8" : "px-4"}>
-          <Outlet />
-          <CreateNoteButton />
+        <div
+          className="bg-gray-100"
+          style={{ flexGrow: 1, overflow: "hidden", maxHeight: "100vh" }}
+        >
+          <Navbar
+            style={{
+              position: "absolute",
+              top: 0,
+              width: "100%",
+              zIndex: 9999,
+            }}
+          />
+          <div className={isNonMobile ? "px-8" : "px-4"}>
+            <Outlet />
+            <CreateNoteButton />
+          </div>
         </div>
       </div>
-    </div>
+    </NotesProvider>
   );
 };
 
