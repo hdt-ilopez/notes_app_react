37b09b490d411c2218f2be33c3c5c0bef581c81d
Izaac Lopez <ilopez@illuminateit.us>
2024-09-02 11:22:17 -0500

    Added mobile logout component

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -6,20 +6,35 @@ const UserAvatar = () => {
   const { notesUser } = useAuth();
   const { logout } = useAuthHooks();
   return (
-    <div className="flex gap-2 items-center text-black">
-      <div tabIndex={0} className="avatar w-[50px]">
-        <img src={notesUser?.profilePicture} className="rounded-full" />
+    <>
+      <div className="hidden gap-2 items-center  md:flex text-black">
+        <div tabIndex={0} className="avatar w-[50px]">
+          <img src={notesUser?.profilePicture} className="rounded-full" />
+        </div>
+        <div className="hidden md:block">
+          {" "}
+          <p className="font-bold capitalize">
+            {notesUser?.firstName} {notesUser?.lastName}
+          </p>
+          <button onClick={logout} className="underline text-sm cursor-pointer">
+            Logout
+          </button>
+        </div>
       </div>
-      <div className="hidden md:block">
-        {" "}
-        <p className="font-bold capitalize">
-          {notesUser?.firstName} {notesUser?.lastName}
-        </p>
-        <button onClick={logout} className="underline text-sm cursor-pointer">
-          Logout
-        </button>
+      <div className="dropdown md:hidden">
+        <div tabIndex={0} className="avatar w-[50px]">
+          <img src={notesUser?.profilePicture} className="rounded-full" />
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-white text-black rounded-box z-[1] w-52 p-2 shadow right-1"
+        >
+          <li onClick={logout}>
+            <a>Logout</a>
+          </li>
+        </ul>
       </div>
-    </div>
+    </>
   );
 };
 
